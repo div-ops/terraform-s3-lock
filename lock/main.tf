8e70be62ddb834a04de395f0d7c82f598bbeba5f
creaticoding <creaticoding@gmail.com>
2022-01-31 02:08:22 +0900

    feat: lock module

---
+++ lock/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# S3 bucket for backend
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.s3_bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = var.dynamodb_table_name
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+variable "s3_bucket_name" {
+  description = "s3 bucket name"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "dynamodb table name"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+}
